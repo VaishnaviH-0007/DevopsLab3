b0e7af33dc3f16adb3b520d7153e90a9a72c8b25
VaishnaviH-0007 <vvce23ise0232@vvce.ac.in>
2026-03-05 16:26:51 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -1,5 +1,5 @@
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println("Hello!,how r u?,wt's up");
+        System.out.println("Hello!,how r u?,wt's up,had breakfast");
     }
 }
